5abd479fe8cb280c14e908f70c7f4babef518f4e
VickeKatt <viktor.hedenius@gmail.com>
2026-08-22 14:11:08 +0200

    Made some apps !NOT DONE!

--- script.js
+++ script.js
@@ -3,66 +3,103 @@ function updateTime() {
     document.getElementById("datetime").textContent = datetime;
 }
 
-
 updateTime();
 setInterval(updateTime, 1000);
 
-
-dragElement(document.getElementById("window"));
 function dragElement(element) {
-    var initialX = 0, initialY = 0, currentX = 0, currentY = 0;
-    if (document.getElementById(element.id + "header")) {
-        document.getElementById(element.id + "header").onmousedown = StartDragging;
-    } else {
-        element.onmousedown = StartDragging;
-    }
+    var initialX = 0;
+    var initialY = 0;
+    var currentX = 0;
+    var currentY = 0;
+
+    var header = element.querySelector(".windowheader");
+
+    header.onmousedown = StartDragging;
 
     function StartDragging(e) {
-        e =e || window.event;
+        e = e || window.event;
         e.preventDefault();
+
         initialX = e.clientX;
         initialY = e.clientY;
+
         document.onmouseup = StopDragging;
         document.onmousemove = DragElement;
     }
+
     function DragElement(e) {
         e = e || window.event;
         e.preventDefault();
+
         currentX = initialX - e.clientX;
         currentY = initialY - e.clientY;
+
         initialX = e.clientX;
         initialY = e.clientY;
+
         element.style.top = (element.offsetTop - currentY) + "px";
         element.style.left = (element.offsetLeft - currentX) + "px";
     }
+
     function StopDragging() {
         document.onmouseup = null;
         document.onmousemove = null;
     }
 }
 
-
-var welcomeScreen = document.querySelector("#window");
+var welcomeScreen = document.querySelector("#welcomewindow");
+var terminalScreen = document.querySelector("#terminalwindow");
 
 var welcomeScreenClose = document.querySelector("#welcomeclose");
-var welcomeScreenOpen = document.querySelector("#welcomeopen");
+var terminalScreenClose = document.querySelector("#terminalclose");
 
+dragElement(welcomeScreen);
+dragElement(terminalScreen);
 
 function closeWindow(element) {
     element.style.display = "none";
 }
 
-
 function openWindow(element) {
     element.style.display = "block";
 }
-
 
 welcomeScreenClose.addEventListener("click", function() {
     closeWindow(welcomeScreen);
 });
 
+terminalScreenClose.addEventListener("click", function() {
+    closeWindow(terminalScreen);
+});
 
-welcomeScreenOpen.addEventListener("click", function() {
+document.querySelector("#welcomeopen").addEventListener("dblclick", function() {
     openWindow(welcomeScreen);
 });
+
+document.querySelector("#terminal").addEventListener("dblclick", function() {
+    openWindow(terminalScreen);
+});
+
+var selectedIcon = undefined;
+
+function selectIcon(element) {
+    if (selectedIcon !== undefined) {
+        selectedIcon.classList.remove("app-selected");
+    }
+
+    element.classList.add("app-selected");
+    selectedIcon = element;
+}
+
+function deselectIcon(element) {
+    element.classList.remove("app-selected");
+    selectedIcon = undefined;
+}
+
+function handleIconTap(element) {
+    if (element.classList.contains("app-selected")) {
+        deselectIcon(element);
+    } else {
+        selectIcon(element);
+    }
+}
